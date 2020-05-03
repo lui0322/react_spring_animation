111fe60756c28f1f2c26784a45d3e0000cf30cc9
marklowieangeles@gmail.com <marklowieangeles@gmail.com>
2020-05-03 23:35:56 +0800

    react animation initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <ul>
+          <li>
+            <NavLink exact to="/" activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <Route
+        render={({ location }) => (
+          <TransitionGroup>
+            <CSSTransition key={location.key} timeout={300} classNames="fade">
+              <Switch location={location}>
+                <Route exact path="/" component={Home} />
+                <Route path="/" component={About} />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )}
+      />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../assets/logo.png";
+
+const About = () => {
+  return (
+    <div className="page">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>React Animation - About</h1>
+      <p>
+        Proident nisi in officia Lorem cillum laborum ea cupidatat id anim enim
+        irure. Velit commodo qui velit aliquip pariatur. Magna eiusmod consequat
+        cupidatat est id excepteur ex incididunt consectetur. Enim sunt elit
+        ipsum mollit magna. Incididunt qui nostrud commodo sint velit eiusmod.
+        Mollit officia dolor eu aliquip. Minim aliquip tempor qui incididunt
+        occaecat et deserunt consectetur et proident esse non do esse.
+      </p>
+    </div>
+  );
+};
+
+export default About;
